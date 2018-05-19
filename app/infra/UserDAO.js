10f2ce949fbe1ee3f982bbcb10c2f9890cbbff97
FabioLucasRomeiro <flromeiroc@gmail.com>
2018-05-19 20:53:34 -0300

    Colocando infos do bd nos DAOs e colocando front

--- app/infra/UserDAO.js
+++ app/infra/UserDAO.js
@@ -3,7 +3,7 @@ function UserDAO(connection) {
 }
 
 UserDAO.prototype.login = function (tipo, username, password, callback) {
-    this._connection.query("select * from ? where nickname=? and password=?", [tipo,username, password], callback);
+    this._connection.query("select * from user where nick=? and pass=?", [tipo,username, password], callback);
 }
 
 module.exports = function() {
